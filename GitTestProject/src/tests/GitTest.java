3251af0d084d1ae3ba7e01e92ac11809beba2dfd
coolkarni03 <girish.kulkarni732@gmail.com>
2020-04-21 12:53:44 +0530

    added new test class

---
+++ GitTestProject/src/tests/GitTest.java
@@ -0,0 +1,10 @@
+package tests;
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This class is created to test git functions in eclipse");
+	}
+
+}
