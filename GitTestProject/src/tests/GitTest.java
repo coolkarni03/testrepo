35c06026d83e12992b92a16cc4fb82f5d2c551ba
coolkarni03 <girish.kulkarni732@gmail.com>
2022-01-07 08:51:50 +0000

    made changes from iPad

--- GitTestProject/src/tests/GitTest.java
+++ GitTestProject/src/tests/GitTest.java
@@ -5,6 +5,7 @@ public class GitTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This class is created to test git functions in eclipse");
+		System.out.println("This is added from ipad");
 	}
 
 }
